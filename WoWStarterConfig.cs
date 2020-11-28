0f6cf39c2e44f3d4698fb789c339a7c670b73af4
dejay <dejay@r3z.me>
2020-11-28 10:16:45 +0100

    fixed startign without config file

--- WoWStarterConfig.cs
+++ WoWStarterConfig.cs
@@ -43,42 +43,48 @@ namespace WoWStarter
 
 		public static WoWStarterConfig load()
 		{
+			WoWStarterConfig config;
+
 			try
 			{
 				JsonSerializerOptions options = new JsonSerializerOptions();
 				options.IncludeFields = true;
 				String jsonString = File.ReadAllText(configFileName);
-				WoWStarterConfig config = JsonSerializer.Deserialize<WoWStarterConfig>(jsonString, options);
-
-				if (config.installPaths == null)
-				{
-					if (config.installPath != null)
-						config.installPaths = new String[] { config.installPath };
-					else
-					{
-						String wowInstall = Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Blizzard Entertainment\World of Warcraft", "InstallPath", null).ToString();
-						config.installPaths = new String[] { wowInstall };
-					}
-				}
-				config.installPath = null;
-				if (config.customLayout == null)
-				{
-					config.customLayout = new Rectangle[5];
-					config.customLayout[0] = new Rectangle(0, 0, 1920, 1080);
-					for (int i = 1; i < config.customLayout.Length; i++)
-						config.customLayout[i] = new Rectangle(i * 320, 0, 320, 180); 
-				}
-				return config;
+				config = JsonSerializer.Deserialize<WoWStarterConfig>(jsonString, options);
 			}
 			catch (FileNotFoundException)
 			{
-				return new WoWStarterConfig();
+				// ignore
+				config = new WoWStarterConfig();
 			}
 			catch (Exception e)
 			{
 				MessageBox.Show("Error while reading config file, try fixing errors or deleting it: " + configFileName + e.ToString());
 				return null;
 			}
+
+			// initialize empty config
+			if (config.installPaths == null)
+			{
+				if (config.installPath != null)
+					config.installPaths = new String[] { config.installPath };
+				else
+				{
+					String wowInstall = Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Blizzard Entertainment\World of Warcraft", "InstallPath", null).ToString();
+					config.installPaths = new String[] { wowInstall };
+				}
+			}
+			config.installPath = null;
+
+			if (config.customLayout == null)
+			{
+				config.customLayout = new Rectangle[5];
+				config.customLayout[0] = new Rectangle(0, 0, 1920, 1080);
+				for (int i = 1; i < config.customLayout.Length; i++)
+					config.customLayout[i] = new Rectangle(i * 320, 0, 320, 180); 
+			}
+			
+			return config;
 		}
 
 		public void save()
